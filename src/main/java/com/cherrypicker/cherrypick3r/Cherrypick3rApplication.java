196d65a44c2b090f765d963e5bdfc4ec8a39c0b8
fing9 <djajsl54@naver.com>
2023-03-28 15:40:49 +0900

    initial commit

---
+++ src/main/java/com/cherrypicker/cherrypick3r/Cherrypick3rApplication.java
@@ -0,0 +1,13 @@
+package com.cherrypicker.cherrypick3r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cherrypick3rApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cherrypick3rApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherrypicker/cherrypick3r/Cherrypick3rApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherrypicker.cherrypick3r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cherrypick3rApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
