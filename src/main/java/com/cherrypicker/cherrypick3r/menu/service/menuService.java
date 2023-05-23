21b3a1e7ba17c67f9aa4815b1ac1620f7833ff97
ebang <dskhe8@gmail.com>
2023-05-24 00:28:00 +0900

    fix : MenuService

--- src/main/java/com/cherrypicker/cherrypick3r/menu/service/menuService.java
+++ src/main/java/com/cherrypicker/cherrypick3r/menu/service/menuService.java
@@ -3,12 +3,12 @@ package com.cherrypicker.cherrypick3r.menu.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class menuService {
-//    private final MenuRepository menuRepository;
-
-
-
-
-}
+//@Service
+//@RequiredArgsConstructor
+//public class menuService {
+////    private final MenuRepository menuRepository;
+//
+//
+//
+//
+//}
