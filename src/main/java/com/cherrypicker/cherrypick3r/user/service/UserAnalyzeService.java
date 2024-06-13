72907462bf4257ad3792c4536be857fcd7704ae6
fing9 <djajsl54@naver.com>
2024-06-13 17:22:29 +0900

    [Refactor]: 유저의 선호 태그를 찾는 함수의 반복되는 코드 변경

--- src/main/java/com/cherrypicker/cherrypick3r/user/service/UserAnalyzeService.java
+++ src/main/java/com/cherrypicker/cherrypick3r/user/service/UserAnalyzeService.java
@@ -3,11 +3,11 @@ package com.cherrypicker.cherrypick3r.user.service;
 import com.cherrypicker.cherrypick3r.component.GameCalc;
 import com.cherrypicker.cherrypick3r.component.TagType;
 import com.cherrypicker.cherrypick3r.user.domain.User;
-import com.cherrypicker.cherrypick3r.user.domain.UserRepository;
 import com.cherrypicker.cherrypick3r.user.dto.UserSimilarityPair;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,13 +16,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserAnalyzeService {
 
-    private final UserRepository userRepository;
+    private final UserSearchService userSearchService;
 
     private final GameCalc gameCalc;
 
     @Transactional
     public Double calcUserPercentile(String userEmail) {
-        List<User> users = userRepository.findAll();
+        List<User> users = userSearchService.findAllUsers();
         List<UserSimilarityPair> userSimilarityPairs = new ArrayList<>();
 
         // 각 유저와 다른 모든 유저의 유사도를 더한 리스트를 만든다.
@@ -55,105 +55,20 @@ public class UserAnalyzeService {
 
     @Transactional
     public List<String> findUserPreferenceTags(String userEmail) {
-        User user = userRepository.findByEmail(userEmail).get();
+        User user = userSearchService.findUserByEmail(userEmail);
 
         List<Double> tags = user.getTag().getTagsByList();
-        List<Long> userClassifys = new ArrayList<>();
-        int tagsLen = tags.size();
-
-        for (int i = 0; i < tagsLen; i++) {
-            if (tags.get(i) <= 2D) {
-                userClassifys.add(0L);
-            } else {
-                userClassifys.add(1L);
-            }
-        }
+        List<Long> userClassifys = tags.stream()
+            .map(tag -> tag <= 2D ? 0L : 1L)
+            .collect(Collectors.toList());
 
         List<String> retTags = new ArrayList<>();
 
-        if (userClassifys.get(0) == 1) {
-            retTags.add(TagType.CTAG001.getDescription());
-        }
-        if (userClassifys.get(1) == 1) {
-            retTags.add(TagType.CTAG002.getDescription());
-        }
-        if (userClassifys.get(2) == 1) {
-            retTags.add(TagType.CTAG003.getDescription());
-        }
-        if (userClassifys.get(3) == 1) {
-            retTags.add(TagType.CTAG004.getDescription());
-        }
-        if (userClassifys.get(4) == 1) {
-            retTags.add(TagType.CTAG005.getDescription());
-        }
-        if (userClassifys.get(5) == 1) {
-            retTags.add(TagType.CTAG006.getDescription());
-        }
-        if (userClassifys.get(6) == 1) {
-            retTags.add(TagType.CTAG007.getDescription());
-        }
-        if (userClassifys.get(7) == 1) {
-            retTags.add(TagType.CTAG008.getDescription());
-        }
-        if (userClassifys.get(8) == 1) {
-            retTags.add(TagType.CTAG009.getDescription());
-        }
-        if (userClassifys.get(9) == 1) {
-            retTags.add(TagType.CTAG010.getDescription());
-        }
-        if (userClassifys.get(10) == 1) {
-            retTags.add(TagType.CTAG011.getDescription());
-        }
-        if (userClassifys.get(11) == 1) {
-            retTags.add(TagType.CTAG012.getDescription());
-        }
-        if (userClassifys.get(12) == 1) {
-            retTags.add(TagType.CTAG013.getDescription());
-        }
-        if (userClassifys.get(13) == 1) {
-            retTags.add(TagType.CTAG014.getDescription());
-        }
-        if (userClassifys.get(14) == 1) {
-            retTags.add(TagType.CTAG015.getDescription());
-        }
-        if (userClassifys.get(15) == 1) {
-            retTags.add(TagType.CTAG016.getDescription());
-        }
-        if (userClassifys.get(16) == 1) {
-            retTags.add(TagType.CTAG017.getDescription());
-        }
-        if (userClassifys.get(17) == 1) {
-            retTags.add(TagType.CTAG018.getDescription());
-        }
-        if (userClassifys.get(18) == 1) {
-            retTags.add(TagType.CTAG019.getDescription());
-        }
-        if (userClassifys.get(19) == 1) {
-            retTags.add(TagType.CTAG020.getDescription());
-        }
-        if (userClassifys.get(20) == 1) {
-            retTags.add(TagType.CTAG021.getDescription());
-        }
-        if (userClassifys.get(21) == 1) {
-            retTags.add(TagType.CTAG022.getDescription());
-        }
-        if (userClassifys.get(22) == 1) {
-            retTags.add(TagType.CTAG023.getDescription());
-        }
-        if (userClassifys.get(23) == 1) {
-            retTags.add(TagType.CTAG024.getDescription());
-        }
-        if (userClassifys.get(24) == 1) {
-            retTags.add(TagType.CTAG025.getDescription());
-        }
-        if (userClassifys.get(25) == 1) {
-            retTags.add(TagType.CTAG026.getDescription());
-        }
-        if (userClassifys.get(26) == 1) {
-            retTags.add(TagType.CTAG027.getDescription());
-        }
-        if (userClassifys.get(27) == 1) {
-            retTags.add(TagType.CTAG028.getDescription());
+        TagType[] tagTypes = TagType.values();
+        for (int i = 0; i < userClassifys.size(); i++) {
+            if (userClassifys.get(i) == 1 && i < tagTypes.length) {
+                retTags.add(tagTypes[i].getDescription());
+            }
         }
 
         return retTags;
