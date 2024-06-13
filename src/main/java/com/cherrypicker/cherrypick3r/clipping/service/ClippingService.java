45145fa3f02fb590abe0ab66df243e341c4ae6c9
fing9 <djajsl54@naver.com>
2024-06-13 21:13:19 +0900

    [Refactor]: Clipping Service의 조회의 책임 분리

--- src/main/java/com/cherrypicker/cherrypick3r/clipping/service/ClippingService.java
+++ src/main/java/com/cherrypicker/cherrypick3r/clipping/service/ClippingService.java
@@ -5,12 +5,12 @@ import com.cherrypicker.cherrypick3r.clipping.domain.ClippingRepository;
 import com.cherrypicker.cherrypick3r.clipping.dto.ClippingDoResponse;
 import com.cherrypicker.cherrypick3r.clipping.dto.ClippingUndoResponse;
 import com.cherrypicker.cherrypick3r.shop.domain.Shop;
-import com.cherrypicker.cherrypick3r.shop.domain.ShopRepository;
 import com.cherrypicker.cherrypick3r.shop.dto.ShopDto;
 import com.cherrypicker.cherrypick3r.shop.dto.ShopSimple;
+import com.cherrypicker.cherrypick3r.shop.service.ShopSearchService;
 import com.cherrypicker.cherrypick3r.user.domain.User;
-import com.cherrypicker.cherrypick3r.user.domain.UserRepository;
 import com.cherrypicker.cherrypick3r.user.dto.UserDto;
+import com.cherrypicker.cherrypick3r.user.service.UserSearchService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,8 +22,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ClippingService {
     private final ClippingRepository clippingRepository;
-    private final UserRepository userRepository;
-    private final ShopRepository shopRepository;
+    private final UserSearchService userSearchService;
+    private final ShopSearchService shopSearchService;
 
     @Transactional
     public Clipping createClipping(Shop shop, User user)
@@ -36,34 +36,30 @@ public class ClippingService {
     @Transactional
     public ClippingDoResponse createClipping(Long shopId, String userEmail)
     {
-        Shop shop = shopRepository.findById(shopId).get();
-        User user = userRepository.findByEmail(userEmail).get();
+        Shop shop = shopSearchService.findShopById(shopId);
+        User user = userSearchService.findUserByEmail(userEmail);
         Clipping clipping = createClipping(shop, user);
 
-        ClippingDoResponse clippingDoResponse = ClippingDoResponse.builder()
+        return ClippingDoResponse.builder()
                 .clippingId(clipping.getId())
                 .shopId(shopId)
                 .userEmail(userEmail)
                 .build();
-
-        return clippingDoResponse;
     }
 
     @Transactional
     public ClippingUndoResponse deleteClipping(Long shopId, String userEmail)
     {
-        Shop shop = shopRepository.findById(shopId).get();
-        User user = userRepository.findByEmail(userEmail).get();
+        Shop shop = shopSearchService.findShopById(shopId);
+        User user = userSearchService.findUserByEmail(userEmail);
         Clipping clipping = clippingRepository.findByShopAndUser(shop, user);
 
         clippingRepository.delete(clipping);
 
-        ClippingUndoResponse clippingUndoResponse = ClippingUndoResponse.builder()
+        return ClippingUndoResponse.builder()
                 .shopId(shopId)
                 .userEmail(userEmail)
                 .build();
-
-        return clippingUndoResponse;
     }
 
     @Transactional
@@ -82,7 +78,7 @@ public class ClippingService {
     @Transactional
     public List<Shop> findClippingShopByUser(String userEmail)
     {
-        User user = userRepository.findByEmail(userEmail).get();
+        User user = userSearchService.findUserByEmail(userEmail);
         List <Clipping> clippingList = clippingRepository.findAllByUser(user);
         List <Shop> clippedShopByUser =  new ArrayList<>();
 
@@ -96,9 +92,7 @@ public class ClippingService {
     @Transactional
     public List<Clipping> findClippingByUser(User user)
     {
-        List <Clipping> clippingList = clippingRepository.findAllByUser(user);
-
-        return clippingList;
+        return clippingRepository.findAllByUser(user);
     }
 
     @Transactional
@@ -124,30 +118,25 @@ public class ClippingService {
 
     @Transactional
     public Long findClippingByUserEmailAndShopId(String userEmail, Long shopId) {
-        Shop shop = shopRepository.findById(shopId).get();
-        User user = userRepository.findByEmail(userEmail).get();
+        Shop shop = shopSearchService.findShopById(shopId);
+        User user = userSearchService.findUserByEmail(userEmail);
 
         Clipping clipping = clippingRepository.findByShopAndUser(shop, user);
 
-        if (clipping != null) {
-            return 1L;
-        }
-        else {
-            return 0L;
-        }
+        return clipping != null ? 1L : 0L;
     }
 
     @Transactional
     public Long findClippingCountByUserEmail(String userEmail) {
-        User user = userRepository.findByEmail(userEmail).get();
+        User user = userSearchService.findUserByEmail(userEmail);
         List<Clipping> clippings = clippingRepository.findAllByUser(user);
 
-        return Long.valueOf(clippings.size());
+        return (long) clippings.size();
     }
 
     @Transactional
     public List<ShopSimple> find3ShopSimpleByUserEmail(String userEmail) {
-        User user = userRepository.findByEmail(userEmail).get();
+        User user = userSearchService.findUserByEmail(userEmail);
         List<Clipping> clippings = clippingRepository.findAllByUser(user);
         List<ShopSimple> shopSimples = new ArrayList<>();
         int cnt = 0;
