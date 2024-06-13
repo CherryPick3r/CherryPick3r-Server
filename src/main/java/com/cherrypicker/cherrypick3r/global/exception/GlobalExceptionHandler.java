8703fc6dd6f39711633a7426cefaa54d26bc6201
fing9 <djajsl54@naver.com>
2024-06-13 16:41:01 +0900

    [Feat]: UserSearchService를 통한 조회 책임분리 추가

--- src/main/java/com/cherrypicker/cherrypick3r/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cherrypicker/cherrypick3r/global/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import com.cherrypicker.cherrypick3r.global.response.enums.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,6 +30,22 @@ public class GlobalExceptionHandler {
         final ApiResponse<Object> response = ApiResponse.failure(ResultCode.INTERNAL_SERVER_ERROR,
             e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = AuthenticationException.class)
+    public ResponseEntity<ApiResponse<Object>> authenticationExceptionExceptionHandler(Exception e) {
+        log.error("AuthenticationException occurred", e);
+        final ApiResponse<Object> response = ApiResponse.failure(ResultCode.BAD_REQUEST,
+            e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> usernameNotFoundExceptionExceptionHandler(Exception e) {
+        log.error("UsernameNotFoundException occurred", e);
+        final ApiResponse<Object> response = ApiResponse.failure(ResultCode.NOT_FOUND,
+            e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = NotEnoughAuthorityException.class)
